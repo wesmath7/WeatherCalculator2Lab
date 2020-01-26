dd90e8c11b7c53ce016df4eaba64fef0551c1618
Doug Durham <ddurham@dontpaniclabs.com>
2020-01-26 13:18:08 -0600

    Created the stating point for lab

---
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WeatherCalculator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Weather Calculator 2");
+
+            int temperature;
+            int windSpeed;
+            double windChill;
+            string input = "";
+
+            // Get the temperature from the user
+            Console.WriteLine("Enter the temperature");
+
+            input = Console.ReadLine();
+
+            temperature = int.Parse(input);
+
+            // Get the windspeed from the user
+            Console.WriteLine("Enter the wind speed");
+
+            input = Console.ReadLine();
+
+            windSpeed = int.Parse(input);
+
+            // Calculate the wind chill
+
+            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+
+            Console.WriteLine("The wind chill is " + windChill);
+        }
+    }
+}
